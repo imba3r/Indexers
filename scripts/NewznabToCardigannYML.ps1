3a491b74f5ceb719de92e9ced3696804113e2fcf
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-01-17 22:06:40 +0000

    additional newznab convert script work

--- scripts/NewznabToCardigannYML.ps1
+++ scripts/NewznabToCardigannYML.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 7 -Modules powershell-yaml
+
 <#
     .SYNOPSIS
         Name: Invoke-NewznabToCardigannYML.ps1
@@ -12,57 +14,229 @@
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory, Position = 1) ]
+    [Parameter(Mandatory, Position = 0)]
     [string]$site,
-    [Parameter(Position = 2) ]
-    [string]$apikey,
-    [Parameter(Position = 3)]
-    [string]$indexer
+    [Parameter(Position = 1)]
+    [string]$indexer,
+    [Parameter(Position = 2)]
+    [string]$privacy
 )
 
 # Generate Caps Call
-[string]$capsCall = ($site + '/api?t=caps&apikey=' + $apikey)
+[string]$capsCall = ($site + '/api?t=caps')
+
+
+function Invoke-NameReplace {
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC')
+}
 
 # Get Data and digest objects
 [xml]$xmlResponse = (Invoke-WebRequest -Uri $capscall -Headers $headers -ContentType 'application/xml' -Method Get).Content
 $rSearchCaps = $xmlResponse.caps.searching
-$rCategories = $xmlResponse.caps.categories
+$rCategories = $xmlResponse.caps.categories.category
 
 # Caps
-[string]$q_search = ($rSearchCaps.search.supportedParams)
-[string]$movie_search = ($rSearchCaps.'movie-search'.supportedParams)
-[string]$tv_search = ($SearchCaps.'tv-search'.supportedParams)
-[string]$book_search = ($rSearchCaps.'book-search'.supportedParams)
-[string]$audio_search = ($rSearchCaps.'music-search'.supportedParams)
+[string]$q_search = "[$($rSearchCaps.search.supportedParams -replace ",",", ")]"
+[string]$movie_search = "[$($rSearchCaps.'movie-search'.supportedParams -replace ",",", ")]"
+[string]$tv_search = "[$($rSearchCaps.'tv-search'.supportedParams -replace ",",", ")]"
+[string]$book_search = "[$($rSearchCaps.'book-search'.supportedParams -replace ",",", ")]"
+[string]$audio_search = "[$($rSearchCaps.'audio-search'.supportedParams -replace ",",", ")]"
 
 # Get Main Categories: ID, Name
-[array]$maincats = @( $rCategories.category.id, $rCategories.category.name )
+[System.Collections.Generic.List[System.Object]]$categories = @()
 
+foreach ($category in $rCategories)
+{
+    $catName = Invoke-NameReplace -Name $category.name
+    $temp = [PSCustomObject][ordered]@{
+        id = $category.id
+        cat = $catName
+        desc = $category.description
+    }
+    $categories.Add($temp)
+}
 
-# Get Sub Categories  Main ID, ID, Name
-# ToDo: Fix this it is creating a new entry for each rather than concating to a base category
-[array]$subcats = @(($rCategories.category.subcat.id.SubString(0, 1) + '000'), $rCategories.category.subcat.id, $rCategories.category.subcat.name)
+foreach ($category in $rCategories.subcat)
+{
+    $catName = Invoke-NameReplace -Name "$($category.description)/$($category.name)"
+    $temp = [PSCustomObject][ordered]@{
+        id = $category.id
+        cat = $catName
+        desc = $category.description
+    }
+    $categories.Add($temp)
+}
 
-
-# Cleanup Common Category naming issues
-# ToDo: Change the find/replaces to an array input
-$maincats = $maincats -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC'
-$subcats = $subcats -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC'
-
-# ToDo: Join to get main cat and create proper subcat names
-#$subcats = $subcats | Join $maincats on id
+[System.Collections.Generic.List[string]]$ymlCategories = @()
+foreach ($category in ($categories | Sort-Object id))
+{
+    # TODO: This is currently causing the output to be a list of strings
+    $ymlCategories.Add("{ id: $($category.id), cat: $($category.cat), desc: $($category.desc) }")
+}
 
 # ToDo: Validate Categories List (Names) - use newznabcats.txt
 # None matching categories (case insensitive) will need to be commented out
 
-# ToDo: Create YML file from given data inputs
+#TODO: This is currently creating strings for each mode and these shouldn't be strings
+$modes = [ordered]@{
+    search = $q_search
+}
 
-# write host for Debugging Only
-Write-Host ('Indexer site is [' + "$site" + ']')
-Write-Host ('Search Caps are [' + "$q_search" + ']')
-Write-Host ('Movie Caps are [' + "$movie_search" + ']')
-Write-Host ('TV Caps are [' + "$tv_search" + ']')
-Write-Host ('Book Caps are [' + "$book_search" + ']')
-Write-Host ('Music Caps are [' + "$audio_search" + ']')
-Write-Host ('Main Cat are [' + "$maincats" + ']')
-Write-Host ('Sub Cat are [' + "$subcats" + ']')
+if ($tv_search -ne "[]")
+{
+    $modes['tv-search'] = $tv_search
+}
+if ($movie_search -ne "[]")
+{
+    $modes['movie-search'] = $movie_search
+}
+if ($book_search -ne "[]")
+{
+    $modes['book-search'] = $book_search
+}
+if ($audio_search -ne "[]")
+{
+    $modes['audio-search'] = $audio_search
+}
+
+#TODO: Finish this off with some if statements as per modes
+$inputs = [ordered]@{
+    t = "{{ .Query.Type }}"
+    apikey = "{{ .Config.apikey }}"
+    q = "{{ .Keywords }}"
+}
+
+$hashTable = [ordered]@{
+    id = "$($xmlResponse.caps.server.title)-yml"
+    name = $($xmlResponse.caps.server.title)
+    description = "$($xmlResponse.caps.server.title) is a $privacy Usenet Indexer"
+    language = "en-us"
+    type = $privacy
+    allowdownloadredirect = $true
+    protocol = "usenet"
+    encoding = "UTF-8"
+    links = @($xmlResponse.caps.server.url)
+    caps = [ordered]@{
+        categorymappings = $ymlCategories
+        modes = $modes
+    }
+    settings = @(
+        [ordered]@{
+            name = "apikey"
+            type = "text"
+            label = "Site API Key"
+        }
+    )
+    search = [ordered]@{
+        ignoreblankinputs = $true
+        paths = @(
+            @{
+                path = "/api/"
+                response = @{
+                    type = "xml"
+                    attribute = "attributes"
+                }
+            }
+        )
+        error = @(
+            @{
+                selector = "error"
+                message = @{
+                    selector = "error"
+                    attribute = "description"
+                }
+            }
+        )
+        inputs = $inputs
+        rows = @{
+            selector = "rss > channel > item"
+        }
+        fields = [ordered]@{
+            title = @{
+                selector = "title"
+            }
+            details = @{
+                selector = "comments"
+            }
+            date = @{
+                selector = "pubDate"
+            }
+            download = @{
+                selector = "link"
+            }
+            description = @{
+                selector = "description"
+            }
+            tvbdid = @{
+                selector = "tvdbid"
+            }
+            imdbid = @{
+                selector = "imdb"
+            }
+            tmdbid = @{
+                selector = "tmdb"
+            }
+            traktid = @{
+                selector = "traktid"
+                optional = $true
+            }
+            genre = @{
+                selector = "genre"
+                optional = $true
+            }
+            year = @{
+                selector = "year"
+                optional = $true
+            }
+            category = @{
+                selector = 'attr[name="category"]:nth-of-type(1)'
+                attribute = "value"
+            }
+            <# CANNOT HAVE DUPLICATE KEYS IN HASHTABLE VALUE - MAY NEED SOME MANUAL BAKER INPUT, alternatively we can just make all this yml and append it as a string
+            category = @{
+                optional = $true
+                selector = 'attr[name="category"]:nth-of-type(2)'
+                attribute = "value"
+            } #>
+            size = @{
+                selector = 'attr[name="size"]'
+                attribute = "value"
+            }
+            grabs = @{
+                selector = 'attr[name="grabs"]'
+                attribute = "value"
+            }
+            poster = @{
+                selector = 'attr[name="poster"]'
+                attribute = "value"
+            }
+            files = @{
+                selector = 'attr[name="files"]'
+                attribute = "value"
+                optional = $true
+            }
+            coverurl = @{
+                selector = 'attr[name="coverurl"]'
+                attribute = "value"
+            }
+        }
+    }
+    download = @{
+        error = @(
+            @{
+                selector = "error"
+                message = @{
+                    selector = "error"
+                    attribute = "description"
+                }
+            }
+        )
+    }
+}
+
+return ($hashTable | ConvertTo-Yaml)
