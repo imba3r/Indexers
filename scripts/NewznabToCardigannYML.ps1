5f7217e56d75f289309da51d1bb87281c3fd1816
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-01-22 00:15:50 -0600

    more script work

--- scripts/NewznabToCardigannYML.ps1
+++ scripts/NewznabToCardigannYML.ps1
@@ -9,7 +9,13 @@
     .NOTES
         This script has been tested on Windows PowerShell 7.1.3
     .EXAMPLE
-    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -apikey "SomeKey" -indexer "nzbplanet"
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "private" -apipath "/api" -outputfile "C:\Development\Code\Prowlarr_Indexers\definitions\v4\nzbplanet.yml" -language "en-US"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "public"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "semi-private"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net
 #>
 
 [CmdletBinding()]
@@ -19,14 +25,21 @@ param (
     [Parameter(Position = 1)]
     [string]$indexer,
     [Parameter(Position = 2)]
-    [string]$privacy
+    [string]$privacy,
+    [Parameter(Position = 3)]
+    [string]$apipath = '/api',
+    [Parameter(Position = 4)]
+    [System.IO.FileInfo]$outputfile = ".$([System.IO.Path]::DirectorySeparatorChar)newznab.yml",
+    [Parameter(Position = 4)]
+    [string]$language = 'en-US'
 )
 
 # Generate Caps Call
-[string]$capsCall = ($site + '/api?t=caps')
+[string]$capsCall = ($site + $apipath + '?t=caps')
 
 
-function Invoke-NameReplace {
+function Invoke-CatNameReplace
+{
     param (
         [Parameter(Mandatory)]
         [string]
@@ -35,208 +48,273 @@ function Invoke-NameReplace {
     return ($Name -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC')
 }
 
+function Invoke-ModesReplace
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'rid', 'TVRageID' -replace 'tmdbid', 'TMDBID' -replace 'tvdbid', 'TVDBID' -replace 'imdbid', 'IMDBIDShort' -replace 'traktid', 'TraktId' -replace 'season', 'Season' -replace 'ep', 'Ep' -replace 'album', 'Album' -replace 'artist', 'Artist' -replace 'label', 'Label' -replace 'genre', 'Genre' -replace 'year', 'Year' -replace 'tvmazeid', 'TVMazeID')
+}
+function Invoke-YMLReplace
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'category2', 'category' -replace '"{ ', '{ ' -replace ' }"', ' }')
+}
 # Get Data and digest objects
+Write-Information 'Requesting Caps'
 [xml]$xmlResponse = (Invoke-WebRequest -Uri $capscall -Headers $headers -ContentType 'application/xml' -Method Get).Content
 $rSearchCaps = $xmlResponse.caps.searching
 $rCategories = $xmlResponse.caps.categories.category
-
+$rServer = $xmlResponse.caps.server
+Write-Information 'Got Caps'
 # Caps
-[string]$q_search = "[$($rSearchCaps.search.supportedParams -replace ",",", ")]"
-[string]$movie_search = "[$($rSearchCaps.'movie-search'.supportedParams -replace ",",", ")]"
-[string]$tv_search = "[$($rSearchCaps.'tv-search'.supportedParams -replace ",",", ")]"
-[string]$book_search = "[$($rSearchCaps.'book-search'.supportedParams -replace ",",", ")]"
-[string]$audio_search = "[$($rSearchCaps.'audio-search'.supportedParams -replace ",",", ")]"
-
-# Get Main Categories: ID, Name
+[string]$q_search = "[$($rSearchCaps.search.supportedParams -replace 'group, ',''  -replace ', ',',' )]"
+[string]$movie_search = "[$($rSearchCaps.'movie-search'.supportedParams -replace ', ',',')]"
+[string]$tv_search = "[$($rSearchCaps.'tv-search'.supportedParams -replace ', ',',')]"
+[string]$book_search = "[$($rSearchCaps.'book-search'.supportedParams -replace ', ',',')]"
+[string]$audio_search = "[$($rSearchCaps.'audio-search'.supportedParams -replace ', ',',')]"
+Write-Information 'Search Caps Built'
+# Get Categories: ID, MappedName, Name
 [System.Collections.Generic.List[System.Object]]$categories = @()
-
+Write-Information 'Building Categories'
 foreach ($category in $rCategories)
 {
-    $catName = Invoke-NameReplace -Name $category.name
+    $catName = Invoke-CatNameReplace -Name $category.name
     $temp = [PSCustomObject][ordered]@{
-        id = $category.id
-        cat = $catName
-        desc = $category.description
+        id   = $category.id
+        cat  = $catName
+        desc = "'$($category.name)'"
     }
     $categories.Add($temp)
-}
-
-foreach ($category in $rCategories.subcat)
-{
-    $catName = Invoke-NameReplace -Name "$($category.description)/$($category.name)"
-    $temp = [PSCustomObject][ordered]@{
-        id = $category.id
-        cat = $catName
-        desc = $category.description
+    Write-Information 'Building Sub-Categories'
+    foreach ($subcategory in $category.subcat)
+    {
+        $subcatName = Invoke-CatNameReplace -Name "$($catName)/$($subcategory.name)"
+        $subtemp = [PSCustomObject][ordered]@{
+            id   = $subcategory.id
+            cat  = $subcatName
+            desc = "'$($catName)/$($subcategory.name)'"
+        }
+        $categories.Add($subtemp)
     }
-    $categories.Add($temp)
 }
-
+Write-Information 'Categories Built'
 [System.Collections.Generic.List[string]]$ymlCategories = @()
 foreach ($category in ($categories | Sort-Object id))
 {
     # TODO: This is currently causing the output to be a list of strings
     $ymlCategories.Add("{ id: $($category.id), cat: $($category.cat), desc: $($category.desc) }")
 }
-
-# ToDo: Validate Categories List (Names) - use newznabcats.txt
-# None matching categories (case insensitive) will need to be commented out
+# TODO: Validate Categories List (Names) - use newznabcats.txt
+# None matching categories (case insensitive) will need to be commented out - fuzzy match if possible?
+Write-Information 'Categories converted to YML'
 
 #TODO: This is currently creating strings for each mode and these shouldn't be strings
 $modes = [ordered]@{
     search = $q_search
 }
 
-if ($tv_search -ne "[]")
+if ($tv_search -ne '[]')
 {
     $modes['tv-search'] = $tv_search
 }
-if ($movie_search -ne "[]")
+if ($movie_search -ne '[]')
 {
     $modes['movie-search'] = $movie_search
 }
-if ($book_search -ne "[]")
+if ($book_search -ne '[]')
 {
     $modes['book-search'] = $book_search
 }
-if ($audio_search -ne "[]")
+if ($audio_search -ne '[]')
 {
     $modes['audio-search'] = $audio_search
 }
 
-#TODO: Finish this off with some if statements as per modes
 $inputs = [ordered]@{
-    t = "{{ .Query.Type }}"
-    apikey = "{{ .Config.apikey }}"
-    q = "{{ .Keywords }}"
+    t      = '{{ .Query.Type }}'
+    apikey = '{{ .Config.apikey }}'
+    q      = '{{ .Keywords }}'
 }
 
-$hashTable = [ordered]@{
-    id = "$($xmlResponse.caps.server.title)-yml"
-    name = $($xmlResponse.caps.server.title)
-    description = "$($xmlResponse.caps.server.title) is a $privacy Usenet Indexer"
-    language = "en-us"
-    type = $privacy
-    allowdownloadredirect = $true
-    protocol = "usenet"
-    encoding = "UTF-8"
-    links = @($xmlResponse.caps.server.url)
-    caps = [ordered]@{
-        categorymappings = $ymlCategories
-        modes = $modes
+foreach ($searchinput in ($modes.GETENUMERATOR() | ForEach-Object { $_.VALUE }).Replace('q', '').Replace('[', '').Replace(']', '').Split(',') | Sort-Object)
+{
+    if ($searchinput -ne 'q' -and $searchinput -ne '')
+    {
+        #return $searchinput
+        [string]$searchstring = $(Invoke-ModesReplace -Name $($searchinput))
+        $inputs.Add($($searchinput), "{{ .Query.$($searchstring)}}")
     }
-    settings = @(
-        [ordered]@{
-            name = "apikey"
-            type = "text"
-            label = "Site API Key"
+}
+Write-Information 'Search Caps converted to YML & Search Inputs created'
+
+$inputs.Add('cat', '{{ join .Categories \", \" }}')
+$inputs.Add('raw', '&extended=1')
+
+if (!$indexer)
+{
+    $indexer = $($rServer.title).replace(' - NZB', '')
+}
+if ($outputfile.Name -eq 'newznab.yml')
+{
+    $outputfile = "../definitions/v4/$([System.IO.Path]::DirectorySeparatorChar)$($indexer.Replace(' ','').ToLower()).yml"
+}
+[string]$indexerstrap = $($rServer.strapline)
+[string]$indexerdescr = "'$($indexer) is a $($privacy.ToUpper()) Newznab Usenet Indexer'"
+if (!$indexerstrap)
+{
+    $indexerdescr = $indexerstrap
+}
+Write-Information 'Building YML'
+$hashTable = [ordered]@{
+    id                    = "$($indexer.Replace(' ','').ToLower())-yml"
+    name                  = $indexer
+    description           = $indexerdescr
+    language              = $language
+    type                  = $privacy.ToLower()
+    allowdownloadredirect = $true
+    protocol              = 'usenet'
+    encoding              = 'UTF-8'
+    links                 = @($rServer.url)
+    caps                  = [ordered]@{
+        categorymappings = $ymlCategories
+        modes            = $($modes)
+    }
+    settings              = @([ordered]@{
+            name  = 'apikey'
+            type  = 'text'
+            label = 'Site API Key'
         }
     )
-    search = [ordered]@{
+    search                = [ordered]@{
         ignoreblankinputs = $true
-        paths = @(
-            @{
-                path = "/api/"
-                response = @{
-                    type = "xml"
-                    attribute = "attributes"
+        paths             = @(
+            @(    
+                [ordered]@{
+                    path     = $apipath
+                    response = [ordered]@{
+                        type      = 'xml'
+                        attribute = 'attributes'
+                    }
                 }
-            }
+            )
         )
-        error = @(
-            @{
-                selector = "error"
-                message = @{
-                    selector = "error"
-                    attribute = "description"
+        error             = @(
+            @(
+                [ordered]@{
+                    selector = 'error'
+                    message  = [ordered]@{
+                        selector  = 'error'
+                        attribute = 'description'
+                    }
                 }
-            }
+            )
         )
-        inputs = $inputs
-        rows = @{
-            selector = "rss > channel > item"
+        inputs            = $inputs
+        rows              = @{
+            selector = 'rss > channel > item'
         }
-        fields = [ordered]@{
-            title = @{
-                selector = "title"
+        fields            = [ordered]@{
+            title       = @{
+                selector = 'title'
             }
-            details = @{
-                selector = "comments"
+            details     = @{
+                selector = 'comments'
             }
-            date = @{
-                selector = "pubDate"
+            date        = @{
+                selector = 'pubDate'
             }
-            download = @{
-                selector = "link"
+            download    = @{
+                selector = 'link'
             }
             description = @{
-                selector = "description"
+                selector = 'description'
             }
-            tvbdid = @{
-                selector = "tvdbid"
+            tvdbid      = @{
+                selector = 'tvdbid'
             }
-            imdbid = @{
-                selector = "imdb"
+            imdbid      = @{
+                selector = 'imdb'
             }
-            tmdbid = @{
-                selector = "tmdb"
+            tmdbid      = @{
+                selector = 'tmdb'
             }
-            traktid = @{
-                selector = "traktid"
+            traktid     = [ordered]@{
+                selector = 'traktid'
                 optional = $true
             }
-            genre = @{
-                selector = "genre"
+            genre       = [ordered]@{
+                selector = 'genre'
                 optional = $true
             }
-            year = @{
-                selector = "year"
+            year        = [ordered]@{
+                selector = 'year'
                 optional = $true
             }
-            category = @{
-                selector = 'attr[name="category"]:nth-of-type(1)'
-                attribute = "value"
+            category    = [ordered]@{
+                selector  = 'attr[name="category"]:nth-of-type(1)'
+                attribute = 'value'
             }
-            <# CANNOT HAVE DUPLICATE KEYS IN HASHTABLE VALUE - MAY NEED SOME MANUAL BAKER INPUT, alternatively we can just make all this yml and append it as a string
-            category = @{
-                optional = $true
-                selector = 'attr[name="category"]:nth-of-type(2)'
-                attribute = "value"
-            } #>
-            size = @{
-                selector = 'attr[name="size"]'
-                attribute = "value"
+            category2   = [ordered]@{
+                optional  = $true
+                selector  = 'attr[name="category"]:nth-of-type(2)'
+                attribute = 'value'
             }
-            grabs = @{
-                selector = 'attr[name="grabs"]'
-                attribute = "value"
+            size        = [ordered]@{
+                selector  = 'attr[name="size"]'
+                attribute = 'value'
             }
-            poster = @{
-                selector = 'attr[name="poster"]'
-                attribute = "value"
+            grabs       = [ordered]@{
+                selector  = 'attr[name="grabs"]'
+                attribute = 'value'
             }
-            files = @{
-                selector = 'attr[name="files"]'
-                attribute = "value"
-                optional = $true
+            poster      = [ordered]@{
+                selector  = 'attr[name="poster"]'
+                attribute = 'value'
             }
-            coverurl = @{
-                selector = 'attr[name="coverurl"]'
-                attribute = "value"
+            files       = [ordered]@{
+                selector  = 'attr[name="files"]'
+                attribute = 'value'
+                optional  = $true
+            }
+            coverurl    = [ordered]@{
+                selector  = 'attr[name="coverurl"]'
+                attribute = 'value'
             }
         }
     }
-    download = @{
+    download              = [ordered]@{
         error = @(
-            @{
-                selector = "error"
-                message = @{
-                    selector = "error"
-                    attribute = "description"
+            @(
+                [ordered]@{
+                    selector = 'error'
+                    message  = [ordered]@{
+                        selector  = 'error'
+                        attribute = 'description'
+                    }
                 }
-            }
+            )
         )
     }
 }
+Write-Information 'YML Built'
 
-return ($hashTable | ConvertTo-Yaml)
+$ymlout = '---
+'
+$ymlout += (Invoke-YMLReplace -Name $($hashTable | ConvertTo-Yaml))
+$ymlout = ($ymlout).replace("'[", '[')
+$ymlout = ($ymlout).replace("]'", ']')
+$ymlout = ((($ymlout) -replace '\\\\', '\') -replace '---', '---').Trim()
+$ymlout += '
+# newznab standard'
+# return $ymlout
+
+Write-Information 'Indexer YML Complete'
+$ymlout | Out-File $OutputFile -Encoding 'UTF-8'
+Write-Information 'Indexer YML Page Output - [$OutputFile]'
